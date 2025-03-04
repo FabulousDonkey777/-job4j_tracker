c5fb3e5240ea29779f7881fd612b2bd78f6311bd
FabulousDonkey777 <oceaniagb1984@gmail.com>
2025-03-04 20:42:47 +0300

    1.7. Конструктор [#173889]

---
+++ job4j_tracker-master/src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,36 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+    public Error() {
+        active = false;
+        status= 1;
+        message = "Бездарь!";
+    }
+
+    public Error (boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println(active);
+        System.out.println(status);
+        System.out.println(message);
+    }
+    public static void main(String[] args) {
+        Error error = new Error();
+        Error positive = new Error(true,0,"Все отлично!");
+        Error neutral  = new Error(false, 0, "Ни рыба, ни мясо!");
+        error.printInfo();
+        System.out.println("");
+        positive.printInfo();
+        System.out.println("");
+        neutral.printInfo();
+    }
+
+
+}
